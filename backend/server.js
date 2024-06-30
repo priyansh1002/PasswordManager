e3b583ff23843af14430d3f6411984adcde6a43a
Priyansh <priyansh2001singh@gmail.com>
2024-06-30 22:02:21 +0530

    MERN stack Password Manager

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const client = new MongoClient(url);
 
 const dbName = 'LockBox';
 const app = express()
-const port = 3000
+const port = process.env.PORT||3000;
 app.use(bodyparser.json())
 app.use(cors())
 
